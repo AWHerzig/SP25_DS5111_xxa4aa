cbd3bae00d884aa1eff88ccd01d7c3bdb3d4c3c9
AWHerzig <xxa4aa@Virginia.edu>
2025-02-17 22:19:02 +0000

    Solid draft of tests, test file linter runs 7.5 which I think is as good as I can get importing bin

--- tests/test_normalize_csv.py
+++ tests/test_normalize_csv.py
@@ -1,9 +1,53 @@
-import sys
-sys.path.append('.')
-import bin.normalize_csv
+"""
+Test the normalize_csv python script
+"""
 
-def test_normalize_csv():
+
+import sys
+import pandas as pd
+import bin.normalize_csv
+sys.path.append('.')
+
+def test_read_wjsgainers_correct_names():
     """
-    To test the normalize_csv module 
+    to test the reading of wjsgainers.csv
+    make sure column names are correct
     """
-    assert 3==3
+    raw_wjsgainers = pd.read_csv('sample_data/wjsgainers.csv')
+    normalized_wjsgainers = bin.normalize_csv.read_wsj_gainers(raw_wjsgainers)
+
+    correct_columns = ["symbol", "price", "price_change", "price_percent_change"]
+    assert list(normalized_wjsgainers.columns) == correct_columns
+
+def test_read_wjsgainers_correct_shape():
+    """
+    to test the reading of wjsgainers.csv
+    make sure the shape is appropriate
+    """
+    raw_wjsgainers = pd.read_csv('sample_data/wjsgainers.csv')
+    normalized_wjsgainers = bin.normalize_csv.read_wsj_gainers(raw_wjsgainers)
+
+    output_shape = normalized_wjsgainers.shape
+    assert output_shape[0] > 0 and output_shape[1] == 4
+
+def test_read_ygainers_correct_names():
+    """
+    to test the reading of ygainers.csv 
+    make sure column names are correct
+    """
+    raw_ygainers = pd.read_csv('sample_data/ygainers.csv')
+    normalized_ygainers = bin.normalize_csv.read_y_gainers(raw_ygainers)
+
+    correct_columns = ["symbol", "price", "price_change", "price_percent_change"]
+    assert list(normalized_ygainers.columns) == correct_columns
+
+def test_read_ygainers_correct_shape():
+    """
+    to test the reading of ygainers.csv
+    make sure the shape is appropriate
+    """
+    raw_ygainers = pd.read_csv('sample_data/ygainers.csv')
+    normalized_ygainers = bin.normalize_csv.read_y_gainers(raw_ygainers)
+
+    output_shape = normalized_ygainers.shape
+    assert output_shape[0] > 0 and output_shape[1] == 4
