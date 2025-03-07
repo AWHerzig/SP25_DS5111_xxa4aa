312a0d144d1b2cf7bbcff26713530825f9d0a240
AWHerzig <xxa4aa@Virginia.edu>
2025-03-07 15:14:05 +0000

    made an archive for sample_data so the new files can go in there, workingish draft of wsj

--- bin/base_gainer.py
+++ bin/base_gainer.py
@@ -53,6 +53,10 @@ class GainerDownloadWSJ(GainerDownload):
         pass
 
     def download(self):
+        result = subprocess.run(["make", "wjsgainers.csv"])
+        self.raw = pd.read_csv('wjsgainers.csv')
+        os.remove('wjsgainers.html')
+        os.remove('wjsgainers.csv')
         print("Downloading WSJ gainers")
 
 
@@ -87,7 +91,7 @@ class GainerProcessYahoo(GainerProcess):
         print("Normalizing yahoo gainers")
 
     def save_with_timestamp(self):
-        path = "ygainers_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
+        path = "sample_data/ygainers_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
         self.normalized.to_csv(path)
         print("Saving Yahoo gainers")
 # ITS OWN SEPARATE FILE
@@ -96,11 +100,17 @@ class GainerProcessWSJ(GainerProcess):
         pass
 
     def normalize(self, raw_df):
-        
+        norm_df = raw_df[["Unnamed: 0", "Last", "Chg", "% Chg"]].\
+            rename(columns = {"Unnamed: 0": "symbol", "Last": "price", \
+				"Chg": "price_change", "% Chg": "price_percent_change"})
 
+        norm_df["symbol"] = norm_df.symbol.apply(lambda element: element.split('(')[1].split(')')[0])
+        self.normalized = norm_df
         print("Normalizing WSJ gainers")
 
     def save_with_timestamp(self):
+        path = "sample_data/wjsgainers_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
+        self.normalized.to_csv(path)
         print("Saving WSJ gainers")
 
 # TEMPLATE
