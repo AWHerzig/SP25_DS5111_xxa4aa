dccaa81f36b7729fd769a48f59dfa4441607d770
AWHerzig <leojohns31@gmail.com>
2025-03-07 12:39:51 -0500

    Cleaning up for linter, pt1

--- bin/gainers/base.py
+++ bin/gainers/base.py
@@ -1,25 +1,40 @@
-import subprocess
+"""
+This module sets up the base classes for downloaders and processors
+"""
 from abc import ABC, abstractmethod
 # DOWNLOADER
 class GainerDownload(ABC):
+    """
+    This class is the base Class for the Downloader
+    """
     def __init__(self):
-        self.url = url
         self.raw = None
 
     @abstractmethod
     def download(self):
+        """
+        The download method will be specified in the specific classes.
+        """
         pass
-    
 
-# PROCESSORS 
+
 class GainerProcess(ABC):
+    """
+    This class is the base Class for the Processors
+    """
     def __init__(self):
         self.normalized = None
 
     @abstractmethod
     def normalize(self, raw_df):
+        """
+        The normalize method will be specified in the specific classes.
+        """
         pass
 
     @abstractmethod
     def save_with_timestamp(self):
+        """
+        The saving method will be specified in the specific classes.
+        """
         pass
--- bin/gainers/factory.py
+++ bin/gainers/factory.py
@@ -1,22 +1,36 @@
-import bin.gainers.yahoo as yahoo
-import bin.gainers.wsj as wsj
+"""
+This module sets up the overall factory for the gainer jobs
+"""
+from bin.gainers import yahoo
+from bin.gainers import wsj
 
 # FACTORY
 class GainerFactory:
+    """
+    This class is the aforementioned factory
+    """
     def __init__(self, choice):
         assert choice in ['yahoo', 'wsj', 'wjs'], f"Unrecognized gainer type {choice}"
-        self.choice = choice 
+        self.choice = choice
 
     def get_downloader(self):
-        # trigger off url to return correct downloader
+        """
+        This method will identify which downloader to use in this job
+        
+        trigger off url to return correct downloader
+        """
         if self.choice == 'yahoo':
             return yahoo.GainerDownloadYahoo()
-        elif self.choice in ['wsj', 'wjs']:
+        else:
             return wsj.GainerDownloadWSJ()
 
     def get_processor(self):
-        # trigger off url to return correct downloader
+        """
+        This method will identify which processor to use in this job
+        
+        trigger off url to return correct downloader
+        """
         if self.choice == 'yahoo':
             return yahoo.GainerProcessYahoo()
-        elif self.choice == 'wsj':
+        else':
             return wsj.GainerProcessWSJ()
